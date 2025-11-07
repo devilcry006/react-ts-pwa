4e93cc0a47dd3aa70522c151b60759c892918614
Chonlawat <chonlawat.sa@odds.team>
2025-11-07 10:21:36 +0700

    fix: service worker file

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register('/pwabuilder-sw.js')
       .then((registration) => {
         console.log('Service Worker registered:', registration);
       })
